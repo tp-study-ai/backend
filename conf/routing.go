92447091ecf0c6c988f58c60028ccc00ddb3805b
yut_fut <seregey.golubev@mail.ru>
2023-03-29 14:03:00 +0300

    edited url for prefix

--- conf/routing.go
+++ conf/routing.go
@@ -10,6 +10,6 @@ type ServerHandlers struct {
 }
 
 func (sh *ServerHandlers) ConfigureRouting(router *echo.Echo) {
-	router.GET("get_task", sh.TaskHandler.GetTask)
-	router.GET("get_task_by_id", sh.TaskHandler.GetTaskById)
+	router.GET("api/get_task", sh.TaskHandler.GetTask)
+	router.GET("api/get_task_by_id", sh.TaskHandler.GetTaskById)
 }
