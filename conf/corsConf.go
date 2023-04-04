a61809f4cc33908838d35bd1a4261fb37acf0123
yut_fut <seregey.golubev@mail.ru>
2023-04-05 00:24:11 +0300

    edited cors conf

--- conf/corsConf.go
+++ conf/corsConf.go
@@ -7,13 +7,14 @@ import (
 
 func getCorsConfig() middleware.CORSConfig {
 	return middleware.CORSConfig{
+		//Skipper: middleware.DefaultSkipper,
 		AllowOrigins: []string{
-			"*",
-			"http://localhost:3000",
-			"https://www.study-ai.ru",
-			"https://study-ai.ru",
-			"http://146.185.208.233:80",
-			"https://146.185.208.233:433",
+			"",
+			//"http://localhost:3000",
+			//"https://www.study-ai.ru",
+			//"https://study-ai.ru",
+			//"http://146.185.208.233:80",
+			//"https://146.185.208.233:433",
 		},
 		AllowCredentials: true,
 		//AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderXCSRFToken},
