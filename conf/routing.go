86f976653afffbe16834875725fb4069644f77f0
yut_fut <seregey.golubev@mail.ru>
2023-04-04 22:57:55 +0300

    edited register handler

--- conf/routing.go
+++ conf/routing.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4"
 	echoMiddleware "github.com/labstack/echo/v4/middleware"
 	"github.com/tp-study-ai/backend/internal/app/task"
+	"net/http"
 )
 
 type ServerHandlers struct {
@@ -11,12 +12,12 @@ type ServerHandlers struct {
 }
 
 func (sh *ServerHandlers) ConfigureRouting(router *echo.Echo) {
-	//router.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-	//	AllowOrigins: []string{"*"},
-	//	AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
-	//	AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
-	//}))
-	router.Use(echoMiddleware.CORSWithConfig(getCorsConfig()))
+	router.Use(echoMiddleware.CORSWithConfig(echoMiddleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}))
+	//router.Use(echoMiddleware.CORSWithConfig(getCorsConfig()))
 
 	router.GET("/api/get_task", sh.TaskHandler.GetTask)
 	router.GET("/api/get_task_by_id", sh.TaskHandler.GetTaskById)
