85cf7a1db47cfec15ae20a05b134312d043eb4a2
yut_fut <seregey.golubev@mail.ru>
2023-04-05 17:04:50 +0300

    added db manager

--- internal/app/task/handler.go
+++ internal/app/task/handler.go
@@ -135,7 +135,6 @@ func (h HandlerTask) CheckSolution(ctx echo.Context) error {
 	var solution models.CheckSolutionRequest
 	if err := ctx.Bind(&solution); err != nil {
 		return tools.CustomError(ctx, err, 1, "")
-		//return ctx.JSONBlob(http.StatusInternalServerError, nil)
 	}
 
 	testisResponse, err := h.UseCase.CheckSolution(solution)
--- tools/customError.go
+++ tools/customError.go
@@ -16,5 +16,5 @@ func CustomError(ctx echo.Context, err error, number int, comment string) error 
 	}
 	result, _ := json.Marshal(che)
 	ctx.Response().Header().Add(echo.HeaderContentLength, strconv.Itoa(len(result)))
-	return ctx.JSONBlob(http.StatusBadRequest, result)
+	return ctx.JSONBlob(http.StatusOK, result)
 }
