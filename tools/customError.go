6f429c50edaa12baad09d902dde35eb3a5e30d89
yut_fut <seregey.golubev@mail.ru>
2023-04-05 16:52:28 +0300

    added custom Error

--- tools/customError.go
+++ tools/customError.go
@@ -16,5 +16,5 @@ func CustomError(ctx echo.Context, err error, number int, comment string) error 
 	}
 	result, _ := json.Marshal(che)
 	ctx.Response().Header().Add(echo.HeaderContentLength, strconv.Itoa(len(result)))
-	return ctx.JSONBlob(http.StatusInternalServerError, result)
+	return ctx.JSONBlob(http.StatusBadRequest, result)
 }
