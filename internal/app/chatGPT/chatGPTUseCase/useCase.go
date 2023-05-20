82d15ce4ea9cd37a91fae28db34db40bd21b4ea6
yut_fut <seregey.golubev@mail.ru>
2023-05-20 19:24:08 +0300

    edited cg

--- internal/app/chatGPT/chatGPTUseCase/useCase.go
+++ internal/app/chatGPT/chatGPTUseCase/useCase.go
@@ -1,14 +1,11 @@
 package chatGPTUseCase
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/pkg/errors"
 	"github.com/tp-study-ai/backend/internal/app/chatGPT"
 	"github.com/tp-study-ai/backend/internal/app/models"
-	"io/ioutil"
-	"net/http"
 )
 
 type UseCaseChatGPT struct {
@@ -24,40 +21,41 @@ func NewUseCaseChatGPT(TaskRepo chatGPT.Repository, secret string) *UseCaseChatG
 }
 
 func (u *UseCaseChatGPT) Chat(Message models.ChatGPT) (*models.Message, error) {
-	fmt.Println(Message.TaskId)
-	task, err := u.Repo.GetTaskForChatGPT(Message.TaskId)
-	if err != nil {
-		return nil, err
-	}
 
-	MessageRequest := &models.ChatGPTRequest{
-		UserMessage:    Message.Message,
-		Statement:      task.Description,
-		UserSolution:   Message.Code,
-		MasterSolution: task.MasterSolution,
-	}
+	fmt.Println(Message)
 
-	result, err := json.Marshal(MessageRequest)
-	if err != nil {
-		return nil, err
-	}
-
-	req := bytes.NewBuffer(result)
-	resp, err := http.Post(u.CGSecret, "application/json", req)
-	if err != nil {
-		return nil, err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	fmt.Println(string(body))
+	//task, err := u.Repo.GetTaskForChatGPT(Message.TaskId)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//MessageRequest := &models.ChatGPTRequest{
+	//	UserMessage:    Message.Message,
+	//	Statement:      task.Description,
+	//	UserSolution:   Message.Code,
+	//	MasterSolution: task.MasterSolution,
+	//}
+	//
+	//result, err := json.Marshal(MessageRequest)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//req := bytes.NewBuffer(result)
+	//resp, err := http.Post(u.CGSecret, "application/json", req)
+	//if err != nil {
+	//	return nil, err
+	//}
+	//
+	//body, err := ioutil.ReadAll(resp.Body)
+	//if err != nil {
+	//	return nil, err
+	//}
 
 	var ChatGPTResponse models.Message
 
-	err = json.Unmarshal(body, &ChatGPTResponse)
+	//err = json.Unmarshal(body, &ChatGPTResponse)
+	err := json.Unmarshal("Привет! Рада видеть, что ты решаешь задачи на обучающей платформе и развиваешь свои навыки.\nЯ вижу что ты решаешь задачу про игру Алисы и Боба, где должен определить победителя.\nНо твоя ошибка заключается в том, что вместо инкремента cnt1 и cnt2 ты их декрементируешь.\nПопробуй поменять свой код следующим образом:\n```cpp\nif (s[i] == '0') {\n    cnt1++;\n} else {\n    cnt2++;\n}```", &ChatGPTResponse)
 	if err != nil {
 		return nil, errors.Errorf("1511 " + err.Error())
 	}
